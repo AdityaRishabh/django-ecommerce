6966705bae0be4ad1c527e1756cb966ecb1fadf0
Rishabh Aditya <2001rishabh116@gmail.com>
2026-04-27 22:28:33 +0530

    updated settings.py and serializers.py for rest framework

--- authproject/accounts/serializers.py
+++ authproject/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Order
+from .models import Product, CartItem, Order, OrderItem
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,7 +8,25 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CartItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'product_name', 'quantity']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+
+    class Meta:
+        model = OrderItem
+        fields = ['product_name', 'quantity']
+
+
 class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(source='orderitem_set', many=True)
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['id', 'total_amount', 'status', 'created_at', 'items']
